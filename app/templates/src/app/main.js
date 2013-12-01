29edec61caa3d2e03e841ec0e2818f2278997055
Bryan Forbes <bryan@reigndropsfall.net>
2013-12-01 16:18:36 -0600

    Add comments detailing when `app/main.init()` executes, how it should be used, and where to place content in index.html

--- app/templates/src/app/main.js
+++ app/templates/src/app/main.js
@@ -2,5 +2,11 @@ define([
 	'exports'
 ], function (<%= _.camelize(appname) %>) {
 	<%= _.camelize(appname) %>.init = function () {
+		//	summary:
+		//		This function is executed automatically by the loader configuration.
+		//		It will be executed after the page has loaded, the DOM is ready, and all
+		//		dependencies of this module have been loaded. Use this function to initialize
+		//		the application; for instance, creating	a page controller or running the
+		//		Dojo parser.
 	};
 });
